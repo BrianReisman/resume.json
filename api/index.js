ef3f3576aaa0835805806cb0090827319ee0b0f2
BrianReisman <brianreisman@protonmail.com>
2021-06-22 22:00:59 -0400

    add / route for testing to index.js

--- api/index.js
+++ api/index.js
@@ -9,6 +9,10 @@ server.get('/api/resume', (req,res) => {
   res.status(200).json({resume: resume})
 })
 
+server.get('/', (req,res) => {
+  res.status(201).json({message: '201 testing'})
+})
+
 module.exports = (req, res) => {
   const { name = 'World' } = req.query
   res.send(`Hello ${name}!`)
