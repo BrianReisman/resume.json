5ae4616736d0c9582c93aed6f847d563e0660b3e
BrianReisman <brianreisman@protonmail.com>
2021-06-22 22:34:24 -0400

    reinstate express

--- api/resume.js
+++ api/resume.js
@@ -1,4 +1,13 @@
-module.exports = (req, res) => {
-  const resume = {name: "Brian Reisman"};
-  res.status(200).send(resume)
-}
+const express = require('express');
+const resume = require('./resume.json');
+
+const server = express();
+server.use(express.json())
+
+server.get('/resume', (req, res) => res.status(200).json({resume: resume}))
+
+server.get('/', (req, res) => res.status(201).json({message: '201 testing'}))
+
+const PORT = process.env.PORT || 5005;
+
+server.listen(PORT, () => console.log(`server listeneing on PORT: ${PORT}` ))
