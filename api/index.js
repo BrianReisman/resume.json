6d448218fc5bf3c898e0a232343a45e4b45eb8fe
BrianReisman <brianreisman@protonmail.com>
2021-06-22 22:10:29 -0400

    plain NodeJS test

--- api/index.js
+++ api/index.js
@@ -1,21 +1,5 @@
-const express = require('express');
 const resume = require('./resume.json');
 
-const server = express();
-
-server.use(express.json())
-
-server.get('/api/resume', (req,res) => {
-  res.status(200).json({resume: resume})
-})
-
-server.get('/', (req,res) => {
-  res.status(201).json({message: '201 testing'})
-})
-
 module.exports = (req, res) => {
-  const { name = 'World' } = req.query
-  res.send(`Hello ${name}!`)
+  res.status(200).json(resume)
 }
-
-module.exports = server;
